4056bf648bca2522b39a47486d5cbafa4d984c7c
ACE | Miku <159443948+Mikuhimono@users.noreply.github.com>
2025-09-07 22:51:04 +0800

    Update view.php

--- php/view.php
+++ php/view.php
@@ -1,14 +1,21 @@
 <?php
-
-/* (⚠ FOR ANDROID/iOS) 
+/* (⚠️ FOR ANDROID/iOS)
 Phone browsers often don’t have a full PDF viewer they can’t render inline, 
-so they automatically download instead. */
+so they automatically download instead.*/
 $userAgent = $_SERVER['HTTP_USER_AGENT'];
 
-// Disable for Android/iOS Phone
-if (preg_match('/(android|iphone|ipad|ipod|blackberry|windows phone)/i', $userAgent)) {
-    die("Access to this device is not allowed to view the RRL");
+// Detect if it's mobile (Android, iOS, etc.)
+$isMobile = preg_match('/(android|iphone|ipad|ipod|blackberry|windows phone|mobile)/i', $userAgent);
+
+// Exceptions: Firefox on Android OR iOS devices
+$isFirefox = stripos($userAgent, 'Firefox') !== false;
+$isIOS = preg_match('/(iphone|ipad|ipod)/i', $userAgent);
+
+// Block all mobiles, except Android Firefox and iOS
+if ($isMobile && !($isFirefox || $isIOS)) {
+    die("Access to this device is not allowed to view the RRL. Please use Firefox (Android) or iPhone, or switch to a Laptop/PC.");
 }
+
 if (isset($_GET['file'])) {
     $filename = basename($_GET['file']);
     $filepath = "../uploads/" . $filename;
@@ -25,4 +32,5 @@ if (isset($_GET['file'])) {
     } else {
         echo "No file specified.";
     }
+
 ?>
