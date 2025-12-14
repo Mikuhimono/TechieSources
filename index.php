790274940de21b5523088aaca99b866a0d5e9b67
ACE | Miku <159443948+Mikuhimono@users.noreply.github.com>
2025-12-14 16:55:56 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ session_start();
 
 // Check if the user is logged in or not
 if (isset($_SESSION['username'])) { // User is already logged in
-    header("Location: html/home.html");
+    header("Location: src/html/home.html");
     exit();
 } else { // User is mot logged in
-    header("Location: html/login.html");
+    header("Location: src/html/login.html");
     exit();
 }
 ?>
